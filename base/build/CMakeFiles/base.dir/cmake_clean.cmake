528342613a84b872f47c01e5c01a53f58f488ed5
mudirt <mudirt@163.com>
2023-03-17 15:40:17 +0800

    cmake erro

---
+++ base/build/CMakeFiles/base.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/mudirt/netcoding/tinyserver/lib/libbase.pdb"
+  "/home/mudirt/netcoding/tinyserver/lib/libbase.so"
+  "CMakeFiles/base.dir/CurrentThread.cc.o"
+  "CMakeFiles/base.dir/Timestamp.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
